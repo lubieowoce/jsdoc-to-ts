91da2986a7e615a5afc7a7b36680cf433c22a575
Janka Uryga <lolzatu2@gmail.com>
2025-05-10 04:23:09 +0200

    implement at-property

--- test/typedef/input.mjs
+++ test/typedef/input.mjs
@@ -5,3 +5,16 @@
  * @typedef {Object} Foo - this is a trailing comment
  */
 const DUMMY = 1;
+
+/**
+ * A description line at the start.
+ * @typedef {Object} SpecialType - creates a new type named 'SpecialType'
+ * @property {string} prop1 - a string property of SpecialType
+ * @property {number} prop2 - a number property of SpecialType.
+ * this comment line will also be attached to prop2.
+ * @property {number=} prop3 - an optional number property of SpecialType
+ * @prop {number} [prop4] - an optional number property of SpecialType
+ * @prop {number} [prop5=42] - an optional number property of SpecialType with default.
+ * this comment line will also be attached to prop5.
+ */
+const DUMMY2 = 1;
--- test/typedef/output.mts
+++ test/typedef/output.mts
@@ -3,3 +3,23 @@
  */
 export type Foo = Object;
 const DUMMY = 1;
+/**
+ * A description line at the start.
+ */
+export type SpecialType = {
+  /** a string property of SpecialType */
+  prop1: string;
+  /** a number property of SpecialType.
+   * this comment line will also be attached to prop2.
+   **/
+  prop2: number;
+  /** an optional number property of SpecialType */
+  prop3?: number;
+  /** an optional number property of SpecialType */
+  prop4?: number;
+  /** an optional number property of SpecialType with default.
+   * this comment line will also be attached to prop5.
+   **/
+  prop5?: number;
+};
+const DUMMY2 = 1;
